d765b9d0c6db777c4f1209021b408b4360905c49
kiko1313 <tmtato574@gmail.com>
2026-01-07 21:07:57 +0100

    Fix: Add ImgBB and image host domains to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,51 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      // ImgBB support
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ibb.co',
+      },
+      // Catbox support
+      {
+        protocol: 'https',
+        hostname: 'files.catbox.moe',
+      },
+      // Imgur support  
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+      },
+      // Placeholder services
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      // YouTube thumbnails
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
